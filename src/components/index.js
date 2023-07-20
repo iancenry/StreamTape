c92fae4f843e1d6b86b8d966bac17cb701892e99
Ian Cenry <iancenry@gmail.com>
2023-07-20 22:28:46 +0300

    add components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from './Navbar';
+import Feed from './Feed';
+import VideoDetail from './VideoDetail';
+import ChannelDetail from './ChannelDetail';
+import SearchFeed from './SearchFeed';
+
+export { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed };
