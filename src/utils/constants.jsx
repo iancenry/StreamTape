5dd77228f3d8d194f73cc544fd57d53e0bf39d13
Ian Cenry <iancenry@gmail.com>
2023-07-20 23:45:30 +0300

    minor edits

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -12,7 +12,7 @@ import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
-export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+export const logo = '/src/assets/logo.png';
 
 
 export const categories = [
@@ -40,5 +40,4 @@ export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
 export const demoChannelTitle = 'JavaScript Mastery';
 export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
-// export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
 export const demoProfilePicture = '/src/assets/buddy_sample.png';
