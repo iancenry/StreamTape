c5f588b9624bbfcfea17d729e0ead1ef77d983b5
Ian Cenry <iancenry@gmail.com>
2023-07-25 23:05:09 +0300

    add Videos component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar, Videos } from '../components';
 
 const Feed = () => {
   return (
@@ -19,7 +20,7 @@ const Feed = () => {
           px: { sx: '0', md: 2 },
         }}
       >
-        Sidebar
+        <Sidebar />
         <Typography
           className="copyright"
           variant="body2"
@@ -27,6 +28,18 @@ const Feed = () => {
         >
           Copyright {new Date().getFullYear()} Vid254
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: '#fff' }}
+        >
+          New <span style={{ color: '#F31503' }}>Videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,16 @@ import VideoDetail from './VideoDetail';
 import ChannelDetail from './ChannelDetail';
 import SearchFeed from './SearchFeed';
 import SearchBar from './SearchBar';
+import Sidebar from './Sidebar';
+import Videos from './Videos';
 
-export { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed, SearchBar };
+export {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+  SearchBar,
+  Sidebar,
+  Videos,
+};
