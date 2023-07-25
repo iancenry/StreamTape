a2e56423569b053002538768e56b67e861b79caf
Ian Cenry <iancenry@gmail.com>
2023-07-26 00:41:03 +0300

    create & import VideoCard, ChannelCard

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,19 @@
-const Videos = () => {
-  return <div>Videos</div>;
+/* eslint-disable react/prop-types */
+import { Box, Stack, Typography } from '@mui/material';
+import { VideoCard, ChannelCard } from '../components';
+
+const Videos = ({ videos }) => {
+  console.log(videos);
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
