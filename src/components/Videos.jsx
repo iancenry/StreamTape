44c60cfd58809be935f78e119cf9c56efebdcd57
Ian Cenry <iancenry@gmail.com>
2023-08-01 00:06:16 +0300

    chore: complete Videos.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,8 +5,8 @@ import { VideoCard, ChannelCard } from '../components';
 const Videos = ({ videos }) => {
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item, index) => (
-        <Box key={index}>
+      {videos.map((item) => (
+        <Box key={item.id}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
