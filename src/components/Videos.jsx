acd30f2c52ecc1724e8217ecd65d1a3709731eb5
Ian Cenry <iancenry@gmail.com>
2023-08-02 23:37:37 +0300

    chore: Videos.jsx style

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,10 +2,17 @@
 import { Box, Stack } from '@mui/material';
 import { VideoCard, ChannelCard } from '../components';
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return 'Loading...';
+
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item) => (
+    <Stack
+      direction={direction || 'row'}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
+      {videos?.map((item) => (
         <Box key={item.id}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
